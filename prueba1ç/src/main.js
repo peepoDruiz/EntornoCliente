a92167692adfcfedb2c497306ea46c5563bfdaa7
peepoDruiz <danruiatu@alu.edu.gva.es>
2025-09-12 15:47:51 +0200

    first commit

---
+++ prueba1ç/src/main.js
@@ -0,0 +1,7 @@
+import * as Calculadora from "./library/calculadora.js";
+
+console.log(Calculadora.sumar(5, 5));
+console.log(Calculadora.restar(10, 7));
+console.log(Calculadora.dividir(5, 0));
+console.log(Calculadora.dividir(85, 5));
+console.log(Calculadora.multiplicar(100, 20));
