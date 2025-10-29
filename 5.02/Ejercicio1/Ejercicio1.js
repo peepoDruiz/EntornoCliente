8dc8f899043b15a7886e827379127ed1367d25e3
peepoDruiz <danruiatu@alu.edu.gva.es>
2025-10-29 16:38:32 +0100

    Ejercicio 1 de la práctica 5.02 acabado.

--- 5.02/Ejercicio1/Ejercicio1.js
+++ 5.02/Ejercicio1/Ejercicio1.js
@@ -1,17 +1,35 @@
 "use strict";
 
 window.onload = () => {
-    console.log("hola")
-    let elemento = document.getElementsByClassName("toggleElement");
-    elemento.addEventListener(
-    "click",
-    () => {
-      saludar();
-    },
-    false
-  );
+    console.log("hola");
+    let elemento1 = document.querySelector(".elemento1");
+    let elemento2 = document.querySelector(".elemento2");
+    let elemento3 = document.querySelector(".elemento3");
+    let contenido1 = document.querySelector(".contenido1");
+    let contenido2 = document.querySelector(".contenido2");
+    let contenido3 = document.querySelector(".contenido3");
 
-    const saludar = () => {
-        console.log("hola")
-    }
-} //Fin del window.onload.
+    elemento1.addEventListener(
+      "click",
+      () => {
+        contenido1.classList.toggle("contenidosMostrados");
+      },
+      false
+    );
+
+    elemento2.addEventListener(
+      "click",
+      () => {
+        contenido2.classList.toggle("contenidosMostrados");
+      },
+      false
+    );
+
+    elemento3.addEventListener(
+      "click",
+      () => {
+        contenido3.classList.toggle("contenidosMostrados");
+      },
+      false
+    );
+}; //Fin del window.onload.
