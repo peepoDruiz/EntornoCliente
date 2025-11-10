5c19a5e97b807168028d8c4a2adc5a236e55695d
Druiz <druizatuesta@gmail.com>
2025-11-10 14:06:46 +0100

    Práctica 5.04 avanzada

--- React/ejercicios-react/src/components/Peliculas/Peliculas.jsx
+++ React/ejercicios-react/src/components/Peliculas/Peliculas.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import "./Peliculas.css";
 
-const Peliculas = () => {
+const Peliculas = (props) => {
   return (
     <div>
       <div class="parent">
         <div class="div1">
-            <h1>Titulo</h1>
+            <h1>{props.titulo}</h1>
         </div>
         <div class="div2">
             <h1>Portada</h1>
         </div>
         <div class="div3">
-            <h1>Resumen</h1>
+            <h1>{props.resumen}</h1>
         </div>
         <div class="div4">
-            <h1>Elenco</h1>
+            <h1>{props.elenco}</h1>
         </div>
       </div>
     </div>
