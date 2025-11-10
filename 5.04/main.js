2f721f153a9fcccb64e11da57b407f5f8d5cdc61
Druiz <druizatuesta@gmail.com>
2025-11-10 20:48:01 +0100

    Práctica 5.04 acabada.

--- 5.04/main.js
+++ 5.04/main.js
@@ -4,10 +4,12 @@ window.onload = () => {
   const piecesDiv = document.getElementById("pieces");
   //Le he pedido a ChatGPT que me diga como guardar las imagenes del html en un array y lo desordene cada vez.
   const images = Array.from(piecesDiv.querySelectorAll("img"));
-  const shuffled = images.sort(() => Math.random() - 0.5);
-  const board = document.getElementById("board");
+  let shuffled = [...images];
+  shuffled = shuffled.sort(() => Math.random() - 0.5);
   piecesDiv.innerHTML = "<h2>Piezas</h2>";
   shuffled.forEach((img) => piecesDiv.appendChild(img));
+
+  const board = document.getElementById("board");
 
   const draggable_elements = document.getElementsByClassName("draggable");
   for (let i = 0; i < draggable_elements.length; i++) {
@@ -36,18 +38,41 @@ window.onload = () => {
     (evento) => {
       evento.preventDefault();
       if (evento.target.classList.contains("droppable")) {
-        console.log(`Soltado en ${evento.target.className}`);
         evento.target.appendChild(
           document.getElementById(evento.dataTransfer.getData("identifier"))
         );
-        
+        checkWin();
       }
     },
     false
   );
 
+  const victoryRoyalMessage = document.getElementById("victory");
+
+
+  const checkWin = () => {
+    let isCorrect = true;
+    for (let i = 1; i <= 9; i++) {
+        const slot = document.getElementById(`div${i}`);
+        const piece = slot.querySelector("img");
+
+        //Si falta alguna pieza o la pieza no coincide con los números del div no está correcto.
+        if (!piece || piece.id !== String(i)) {
+            isCorrect = false;
+            break;
+        }
+    }
+    //Si es correcto muestra imagen de victoria.
+    if (isCorrect) {
+    victoryRoyalMessage.style.display = "block";
+  }
+  }
+
+  
+  //Quita las imagenes del tablero y las devuelve al div en el que estaban originalmente.
   document.getElementById("reset").addEventListener("click", () => {
-    //Le he preguntado a ChatGPT si había una función de recargar la página. No sé si recargar la página es una mala práctica.
-    location.reload();
+    board.querySelectorAll("div").forEach(div => div.innerHTML = "");
+    shuffled.forEach(img => piecesDiv.appendChild(img));
+    victoryRoyalMessage.style.display = "none";
   });
 }; //Fin del window.onload.
