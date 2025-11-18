47f7285cb1796b0359d7b4fcb31e38184b7046f2
peepoDruiz <danruiatu@alu.edu.gva.es>
2025-11-18 19:54:30 +0100

    Practica 5.05 avanzadisima

--- 5.05/main.js
+++ 5.05/main.js
@@ -1,26 +1,18 @@
 "use strict";
 
-import { validateTextLength, validateNumber, validateRegExp, validateForm } from "./libraries/library.js";
+import { validateForm, getFormData } from "./libraries/library.js";
 
 window.onload = () => {
     const form = document.forms.formAlbum;
-    console.log(form);
 
-    let nombre = "ranas";
-    console.log(validateTextLength(nombre, 5));
-    let numerito = 2004;
-    console.log(validateNumber(numerito, 1887, 2025));
+    const saveForm = document.getElementById("saveAlbum");
 
-    const expLocation = /^ES-[0-9]{3}[A-Z]{2}$/;
-    let probarRegExp = "ES-001AA"
-    console.log("RegExp")
-    console.log(validateRegExp(probarRegExp, expLocation));
-
-    const saveAlbum = document.getElementById("saveAlbum");
-
-    saveAlbum.addEventListener(
+    saveForm.addEventListener (
         "click",
-        validateForm(form),
-        false
-    );
+        () => {
+            if (validateForm(form)) {
+                getFormData(form);
+            }
+        }
+    )
 } //Fin del window.onload.
